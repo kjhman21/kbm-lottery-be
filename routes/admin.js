bf0feaebb2c9b5d1eccbd0324e7f12e48561e4c2
Junghyun Colin Kim <colin.klaytn@krustuniverse.com>
2022-12-15 22:42:31 +0900

    update uuid api

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,41 @@ const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const { DB } = require('../models');
 const mintStatusEnum = require('../common/mint_status_enum');
+const { checkDefined } = require('../utils/common');
 
 router.post('/uuid', async (req, res) => {
     try {
-        var n = req.body.num;
-        for(var i = 0;i < n; i++) {
+        var klaytnAddress = req.body.klaytnAddress;
+        var signature = req.body.signature;
+        var message = req.body.message;
+        var timestamp = req.body.timestamp;
+        var num = req.body.num;
+
+        checkDefined(req.body, [
+            'num',
+            'klaytnAddress',
+            'signature',
+            'message',
+        ])
+
+        if(message !== "uuid") {
+            throw "wrong message"
+        }
+        
+        if(!validateTimestamp(timestamp)) {
+            throw "timestampe valid"
+        }
+
+        message += timestamp;
+
+        // verify signature with klaytnAddress.
+        var caver = new Caver(process.env.KLAYTN_ENDPOINT)
+        var decodedSignature = caver.utils.decodeSignature(signature)
+        if(! await caver.validator.validateSignedMessage(message, decodedSignature, klaytnAddress)) {
+            throw "signature verification failed";
+        }
+
+        for(var i = 0;i < num; i++) {
             var uuid = uuidv4();
 
             DB.MintStatus.create({
